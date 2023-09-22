dca1da7b27a91399a209642743e4488dc4abbce7
ctao <tao.chupin@ynov.com>
2023-09-22 22:37:25 +0200

    add tp1.2 sum and factorial

---
+++ src/modules/chupin_pawlonka_TP1_B2/tp1_2_sum_factorial.py
@@ -0,0 +1,44 @@
+class SumAndFactorial:
+	def __init__(self, number, result, list_numbers):
+		self.number = number
+		self.result = result
+		self.list_numbers = list_numbers
+
+	@property
+	def number(self):
+		return self.number
+
+	@number.setter
+	def number(self, value):
+		self.number = value
+		pass
+
+	@property
+	def result(self):
+		return self.result
+
+	@result.setter
+	def result(self, value):
+		self.result = value
+		pass
+
+	@property
+	def list_numbers(self):
+		return self.list_numbers
+
+	@list_numbers.setter
+	def list_numbers(self, values):
+		self.list_numbers = values
+		pass
+
+	def sum(self, self.number, self.result):
+		for x in range(self.number):
+			self.result += x
+			pass
+		pass
+
+	def factorial(self, self.number, self.result):
+		for x in range(self.number):
+			self.result *= x
+			pass
+		pass
