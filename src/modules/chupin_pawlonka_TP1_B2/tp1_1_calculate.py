9fc5230b6ed208dc79b2d9e087a4a11630f9f16e
ctao <tao.chupin@ynov.com>
2023-09-22 22:36:47 +0200

    add tp1.1 calculate

---
+++ src/modules/chupin_pawlonka_TP1_B2/tp1_1_calculate.py
@@ -0,0 +1,38 @@
+class Calculate:
+	def __init__(self, number, result):
+		self.number = number
+		self.result = result
+
+	@property
+	def number(self):
+		return self.number
+
+	@number.setter
+	def number(self, values):
+		self.number = values
+		pass
+
+	@property
+	def result(self):
+		return self.result
+
+	@result.setter
+	def result(self, value):
+		self.result = value
+		pass
+
+	def addition(self, self.number, self.result):
+		self.result += x
+		pass
+
+	def substraction(self, self.number, self.result):
+		self.result -= x
+		pass
+
+	def multiplication(self, self.number, self.result):
+		self.result *= x
+		pass
+
+	def division(self, self.number, self.result):
+		self.result /= x
+		pass
